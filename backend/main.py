cf5e068de8f1f8ebd302f3727f2172da694a8bb3
s2322087 <s2322087@stu.musashino-u.ac.jp>
2025-08-02 17:04:02 +0900

    デプロイようにパスを変更

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-import models, schemas, database  # 相対インポートを絶対インポートに変更
+from . import models, schemas, database  # 相対インポートを絶対インポートに変更
 import os
 from dotenv import load_dotenv
 
